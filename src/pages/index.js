bdff5b3802307b46486ac10456dfd3731c7f1435
Fernando Cutire ubuntu <fernandocutire@gmail.com>
2021-11-19 01:25:51 -0500

    FIX: Head - problemas con el head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
@@ -9,15 +10,25 @@ import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
   return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      <Timeline />
-    </Layout>
+    <div>
+      <Head>
+        <title>Fernando Cutire Website</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta
+          name='description'
+          content='Fernando Cutire Personal Profesional Website. Software Engineer developing apps on the web , helping business with cloud technologies and improve their processes with software.'
+        />
+      </Head>
+      <Layout>
+        <Section grid>
+          <Hero />
+          <BgAnimation />
+        </Section>
+        <Projects />
+        <Technologies />
+        <Timeline />
+      </Layout>
+    </div>
   );
 };
 
